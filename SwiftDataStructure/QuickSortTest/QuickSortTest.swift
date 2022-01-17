ca75d2a9ca1b439d622ef6ff1af8da8a811310ea
starpro123 <starpro123@naver.com>
2022-01-17 14:34:43 +0900

    퀵소트 완성

--- SwiftDataStructure/QuickSortTest/QuickSortTest.swift
+++ SwiftDataStructure/QuickSortTest/QuickSortTest.swift
@@ -8,9 +8,13 @@
 import XCTest
 
 class QuickSortTest: XCTestCase {
-
     func test_정수_정렬() throws {
-        XCTAssertEqual(QuickSort.sort(collection: [1, 2, 3]), [1, 2, 3])
-        XCTAssertEqual(QuickSort.sort(collection: [2, 1, 3]), [1, 2, 3])
+        XCTAssertEqual(QuickSort().sorted([2, 1]), [1, 2])
+        XCTAssertEqual(QuickSort().sorted([1, 2]), [1, 2])
+        XCTAssertEqual(QuickSort().sorted([1, 2, 3]), [1, 2, 3])
+        XCTAssertEqual(QuickSort().sorted([2, 1, 3]), [1, 2, 3])
+        XCTAssertEqual(QuickSort().sorted([1, 2, 2, 3, 7, 4, 4, 2, 1]), [1, 2, 2, 3, 7, 4, 4, 2, 1].sorted())
+        let testArray = (1...1000).reversed() + Array(1...10000)
+        XCTAssertEqual(QuickSort().sorted(testArray), testArray.sorted())
     }
 }
--- SwiftDataStructure/SwiftDataStructure/SwiftAlgorithm/QuickSort.swift
+++ SwiftDataStructure/SwiftDataStructure/SwiftAlgorithm/QuickSort.swift
@@ -7,9 +7,38 @@
 
 import Foundation
 
+// MARK: 포인터를 이용해 재귀 적으로 만드는 C 예제와는 달리, 불변성을 지키느라 분할정복방식을 이용해 속도가 조금 느림
 struct QuickSort<Element: Comparable> {
     typealias Collection = [Element]
-    static func sort(collection: Collection) -> Collection? {
-        return nil
+    
+    func sorted(_ collection: Collection) -> Collection {
+        if collection.count <= 1 {
+            return collection
+        }
+        let (collection, pivot) = partition(collection: collection)
+        let result =
+            sorted(Array(collection[0..<pivot]))
+            + [collection[pivot]]
+            + sorted(Array(collection[(pivot + 1)...]))
+        return result
+    }
+    
+    private func partition(collection: Collection) -> (collection: Collection, pivot: Int) {
+        var collection = collection
+        let pivot = 0
+        var (l, r) = (1, collection.count - 1)
+        while l <= r {
+            while l < collection.count && collection[l] <= collection[pivot] {
+                l += 1
+            }
+            while r > pivot && collection[r] >= collection[pivot] {
+                r -= 1
+            }
+            if l <= r {
+                (collection[l], collection[r]) = (collection[r], collection[l])
+            }
+        }
+        (collection[pivot], collection[r]) = (collection[r], collection[pivot])
+        return (collection, r)
     }
 }
